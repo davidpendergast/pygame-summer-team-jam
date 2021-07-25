aa17de53f44ab1e93c0b4d05e67142bcec20240a
dpendergast <dlp75@case.edu>
2021-07-25 18:28:17 -0400

    got basic 3d rendering set up

--- rendering/threedee.py
+++ rendering/threedee.py
@@ -4,6 +4,7 @@ import numpy
 import pygame
 
 from pygame import Vector3, Vector2
+import math
 
 
 class Line3D:
@@ -24,18 +25,78 @@ class Line2D:
         self.width = width
 
 
+# base code~
+def ortho_matrix(left, right, bottom, top, near_val, far_val):
+    res = numpy.identity(4, dtype=numpy.float32)
+    res.itemset((0, 0), float(2 / (right - left)))
+    res.itemset((1, 1), float(2 / (top - bottom)))
+    res.itemset((2, 2), float(-2 / (far_val - near_val)))
+
+    t_x = -(right + left) / (right - left)
+    t_y = -(top + bottom) / (top - bottom)
+    t_z = -(far_val + near_val) / (far_val - near_val)
+    res.itemset((0, 3), float(t_x))
+    res.itemset((1, 3), float(t_y))
+    res.itemset((2, 3), float(t_z))
+
+    return res
+
+
+# base code~
+def perspective_matrix(fovy, aspect, z_near, z_far):
+    f = 1 / math.tan(fovy / 2)
+    res = numpy.identity(4, dtype=numpy.float32)
+    res.itemset((0, 0), f / aspect)
+    res.itemset((1, 1), f)
+    res.itemset((2, 2), (z_far + z_near) / (z_near - z_far))
+    res.itemset((3, 2), (2 * z_far * z_near) / (z_near - z_far))
+    res.itemset((2, 3), -1)
+    res.itemset((3, 3), 0)
+    return res
+
+
+# base code~
+def get_matrix_looking_at(eye_xyz, target_xyz, up_vec):
+    n = eye_xyz - target_xyz
+    n.scale_to_length(1)
+    u = up_vec.cross(n)
+    v = n.cross(u)
+    res = numpy.array([[u[0], u[1], u[2], (-u).dot(eye_xyz)],
+                       [v[0], v[1], v[2], (-v).dot(eye_xyz)],
+                       [n[0], n[1], n[2], (-n).dot(eye_xyz)],
+                       [0, 0, 0, 1]], dtype=numpy.float32)
+    return res
+
+
 class Camera3D:
 
-    def __init__(self, style="ortho"):
+    def __init__(self):
         self.position = Vector3(0, 0, 0)
-        self.forward: Vector3 = Vector3(0, 0, 1)
+        self.direction: Vector3 = Vector3(0, 0, 1)
         self.up: Vector3 = Vector3(0, -1, 0)
-        self.fov: float = 45  # horizontal field of view
-        self.style = style
+        self.fov_degrees: float = 45  # vertical field of view
+
+    def __repr__(self):
+        return "{}(pos={}, dir={})".format(type(self).__name__, self.position, self.direction)
 
     def xform_pt(self, surface_size, pt: Vector3) -> Vector2:
-        if self.style == "test":
-            return Vector2(surface_size[0] // 2 + pt.x - self.position[0], surface_size[1] // 2 - pt.y + self.position[1])
+        view_mat = get_matrix_looking_at(self.position, self.position + self.direction, self.up)
+        proj_mat = perspective_matrix(self.fov_degrees / 180 * math.pi, surface_size[0] / surface_size[1], 0.5, 100000)
+        world_v = numpy.array([pt[0], pt[1], pt[2], 1], dtype=numpy.float32)
+        screen_v = proj_mat @ view_mat @ world_v
+
+        if screen_v[3] == 0:
+            # not sure what this means, but it's not good
+            return None
+        x = screen_v[0] / screen_v[3]
+        y = screen_v[1] / screen_v[3]
+        z = screen_v[2] / screen_v[3]
+        if z < 0:
+            # means point is "behind" the camera
+            return None
+        else:
+            return Vector2(surface_size[0] // 2 + surface_size[0] * x,
+                           surface_size[1] // 2 + surface_size[1] * y)
 
     def project_to_surface(self, surface, lines: Iterable[Line3D]) -> List[Line2D]:
         res = []
@@ -71,12 +132,12 @@ if __name__ == "__main__":
 
     pygame.init()
 
-    screen = pygame.display.set_mode((600, 600))
+    screen = pygame.display.set_mode((600, 300), pygame.RESIZABLE)
 
     clock = pygame.time.Clock()
 
-    camera = Camera3D(style="test")
-    camera.position = Vector3(0, 0, 0)
+    camera = Camera3D()
+    camera.position = Vector3(0, 0, -50)
     p1 = Vector3(10, 15, 30)
     p2 = Vector3(10, -20, 30)
     p3 = Vector3(-20, 15, 30)
@@ -101,10 +162,41 @@ if __name__ == "__main__":
             if e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_ESCAPE:
                     sys.exit(0)
+                elif e.key == pygame.K_i:
+                    print("camera = " + str(camera))
+
+        keys_held = pygame.key.get_pressed()
+        if keys_held[pygame.K_LEFT] or keys_held[pygame.K_RIGHT]:
+            xz = Vector2(camera.direction.x, camera.direction.z)
+            xz = xz.rotate(1 * (1 if keys_held[pygame.K_LEFT] else -1))
+            camera.direction.x = xz[0]
+            camera.direction.z = xz[1]
+            camera.direction.scale_to_length(1)
+
+        if keys_held[pygame.K_UP] or keys_held[pygame.K_DOWN]:
+            camera.direction.y += 0.01 * (1 if keys_held[pygame.K_UP] else -1)
+            camera.direction.scale_to_length(1)
+
+        ms = 0.25
+        xz = Vector2(camera.position.x, camera.position.z)
+        view_xz = Vector2(camera.direction.x, camera.direction.z)
+        view_xz.scale_to_length(1)
+
+        if keys_held[pygame.K_a]:
+            xz = xz + ms * view_xz.rotate(90)
+        if keys_held[pygame.K_d]:
+            xz = xz + ms * view_xz.rotate(-90)
+        if keys_held[pygame.K_w]:
+            xz = xz + ms * view_xz
+        if keys_held[pygame.K_s]:
+            xz = xz + ms * view_xz.rotate(180)
+        camera.position.x = xz[0]
+        camera.position.z = xz[1]
+
         screen.fill((0, 0, 0))
 
         angle += 1
-        lines = gen_cube(angle, 100, Vector3(0, 0, 300))
+        lines = gen_cube(angle, 10, Vector3(0, 0, 20))
 
         lines_2d = camera.project_to_surface(screen, lines)
         for l in lines_2d:

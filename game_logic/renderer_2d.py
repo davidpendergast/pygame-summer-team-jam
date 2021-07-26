8af7490bccbb050b08bc68ca7437c4cb9930eb67
tank king <72032294+tank-king@users.noreply.github.com>
2021-07-26 15:00:12 +0530

    Add files via upload

---
+++ game_logic/renderer_2d.py
@@ -0,0 +1,87 @@
+import random
+
+import pygame
+
+# assumption
+# horizontal -> x-axis
+# vertical -> y-axis
+# into-the-screen-axis -> z-axis
+import player_2d
+import utility_functions
+
+
+class Level:
+    def __init__(self, player: player_2d.Player, sides=6):
+        self.lanes = sides
+        self.player = player
+        self.length = 1000
+        self.craters = [[random.randint(0, 5), random.randint(150, self.length - 200)] for _ in range(10)]  # format -> [lane, z-coordinate]
+
+    def update(self):
+        for i in self.craters:
+            player_rect = pygame.Rect(100 + self.player.lane * 100 + 25, self.player.z, 50, 50)
+            crater_rect = pygame.Rect(100 + i[0] * 100 + 25, i[1], 50, 50)
+            if player_rect.colliderect(crater_rect) and self.player.y < 1:
+                self.player.collided = True
+                break
+            else:
+                self.player.collided = False
+
+
+class Renderer:
+    def __init__(self, display: pygame.Surface, level: Level):
+        self.display = display
+        self.level = level
+
+    def update(self, events):
+        self.level.update()
+
+    def render_craters(self):
+        for i in self.level.craters:
+            pygame.draw.rect(self.display, (255, 0, 0), (100 + 25 + i[0] * 100, 600 - 100 - i[1] - 0 + self.level.player.z, 50, 50), 2)
+            utility_functions.Text(self.display, 'Z: ' + str(i[1]), 100 + i[0] * 100, 600 - 100 - i[1] + self.level.player.z, 15).draw()
+
+    def render_player(self):
+        pygame.draw.rect(self.display, (255, 255, 0), (self.level.player.lane * 100 + 100 + 25, 600 - 100 - 0, 50, 50), 2)
+
+    def render_level(self):
+        for i in range(self.level.lanes):
+            pygame.draw.rect(self.display, (0, 0, 255), (100 + i * 100, 600 - 100 - self.level.length + self.level.player.z, 100, self.level.length), 2)
+        self.render_craters()
+        self.render_player()
+
+
+def preview_code():
+    import sys
+    import utility_functions
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+
+    clock = pygame.time.Clock()
+
+    p = player_2d.Player()
+    level1 = Level(p)
+    r = Renderer(screen, level1)
+
+    while True:
+        events = pygame.event.get()
+        for e in events:
+            if e.type == pygame.QUIT:
+                sys.exit(0)
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+        screen.fill((0, 0, 0))
+        r.update(events)
+        p.update(events)
+        r.render_level()
+        p.draw(screen)
+        utility_functions.Text(screen, 'FPS: ' + str(int(clock.get_fps())), 25, 25, 25).draw()
+        pygame.display.update()
+        pygame.display.set_caption(str(int(clock.get_fps())))
+        clock.tick(60)
+
+
+preview_code()
---
+++ game_logic/utility_functions.py
@@ -0,0 +1,80 @@
+import math
+from typing import Union
+import pygame
+import time
+# import config
+
+pygame.init()
+
+
+def distance(p1: Union[tuple, list], p2: Union[tuple, list]) -> float:
+    return math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+
+
+def get_display_resolution() -> tuple[int, int]:
+    display_width: int = pygame.display.Info().current_w
+    display_height: int = pygame.display.Info().current_h
+    return display_width, display_height
+
+
+class SpriteSheet:
+    def __init__(self, img_file_name: Union[str, pygame.Surface], sprite_qty, row, col, color_key=None, flipped=False, scale_factor=1):
+        self.scale_factor = scale_factor
+        self.is_flipped = flipped
+        self.sheet = pygame.image.load(img_file_name) if type(img_file_name) is str else img_file_name
+        self.row = row
+        self.col = col
+        self.w = self.sheet.get_width() // self.col
+        self.h = self.sheet.get_height() // self.row
+        self.color_key = color_key
+        self.sprite_qty = sprite_qty
+        self.sprites = []
+
+    def get_sprite_at_pos(self, x, y):
+        img = pygame.Surface((self.w, self.h), pygame.SRCALPHA)
+        img.blit(self.sheet, (0, 0), pygame.Rect(x, y, self.w, self.h))
+        img = pygame.transform.scale(img, (img.get_width() * self.scale_factor, img.get_height() * self.scale_factor))
+        if self.color_key is not None:
+            img.set_colorkey(self.color_key)
+        if self.is_flipped:
+            return pygame.transform.flip(img, True, False)
+        else:
+            return img
+
+    def get_images(self):
+        c = 0
+        images = []
+        for i in range(self.row * self.col):
+            c += 1
+            if c > self.sprite_qty:
+                break
+            sprite = self.get_sprite_at_pos((i % self.col) * self.w, i % self.row * self.h)
+            images.append(sprite)
+        return images
+
+
+class Text:
+    def __init__(self, display: pygame.Surface, msg, x=250, y=250, size=50, color=(255, 255, 255), font='courier', blink=False, centered=False):
+        self.display = display
+        self.msg = msg
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+        text_font = pygame.font.Font('VectorBattle-e9XO.ttf', self.size)
+        self.text = text_font.render(self.msg, False, self.color)
+        self.blink = blink
+        self.blink_timer = time.time()
+        self.visible = True
+        self.centered = centered
+
+    def draw(self):
+        if self.blink:
+            if time.time() - self.blink_timer >= 0.5:
+                self.blink_timer = time.time()
+                self.visible = not self.visible
+        if self.visible:
+            if self.centered:
+                self.display.blit(self.text, self.text.get_rect(center=(self.x, self.y)))
+            else:
+                self.display.blit(self.text, self.text.get_rect(topleft=(self.x, self.y)))
